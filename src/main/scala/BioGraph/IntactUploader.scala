0c220e63468c6d14f89b5d4d25d3c04250e08fa2
arc7an <arc7an@gmail.com>
2016-06-26 18:18:04 +0300

    IntactUtil. A method for uploading Reactants from Interactors of the Intact PSI-XML. Tries to find existing Polypeptides by XRef. But still tries to find only by one XRef.

--- src/main/scala/BioGraph/IntactUploader.scala
+++ src/main/scala/BioGraph/IntactUploader.scala
@@ -1,22 +1,30 @@
 package BioGraph
 
+import java.io.File
+
 
 /**
   * Created by artem on 16.06.16.
   */
 object IntactUploader {
   def main(args: Array[String]) {
-    val dir = "/home/artem/BLAST_DB/intact/1173018.xml"
-    val ia = new IntactUtil(dir)
+//    val dir = "/home/artem/BLAST_DB/intact/1173018.xml"
+    val dir = "/home/artem/BLAST_DB/intact/ecoli/ecoli_04.xml"
+    val dataBaseFile = new File("/home/artem/work/reps/neo4j-2.3.1/neo4j-community-2.3.1/data/graph.db")
+    val ia = new IntactUtil(dir, dataBaseFile)
     val interactors = ia.getInteractors
-    val i = interactors.head.map(ia.interactorInfo)
-    i.foreach(println)
+    val i = interactors.map(ia.interactorInfo)
+//    i.foreach(println)
     val reactions = ia.getInteractions
-    val r = reactions.head.map(ia.interactionInfo)
-    r.foreach(println)
+    val r = reactions.map(ia.interactionInfo)
+//    r.foreach(println)
     val experiments = ia.getExperiments
-    val e = experiments.head.map(ia.experimentInfo)
-    e.foreach(println)
+    val e = experiments.map(ia.experimentInfo)
+//    e.foreach(println)
+    val foundNodes = ia.findPolypetidesInteractors(interactors)
+//    foundNodes.foreach(println)
+//    println(ia.gbs)
+    ia.createInteractorNodes(interactors)
   }
 }
 
--- src/main/scala/BioGraph/IntactUtil.scala
+++ src/main/scala/BioGraph/IntactUtil.scala
@@ -1,42 +1,68 @@
 package BioGraph
 
+import org.neo4j.graphdb.DynamicLabel
+import org.neo4j.graphdb.factory.GraphDatabaseFactory
 import psidev.psi.mi.xml.io.impl.PsimiXmlReader253
-import psidev.psi.mi.xml.model.{ExperimentDescription, ExperimentalInteractor, Interaction, Interactor, EntrySet}
+import psidev.psi.mi.xml.model.{DbReference, ExperimentDescription, ExperimentalInteractor, Interaction, Interactor, EntrySet}
 import psidev.psi.mi.xml.{PsimiXmlWriter, PsimiXmlReader, PsimiXmlLightweightReader}
 import psidev.psi.mi.xml.xmlindex.impl.PsimiXmlPullParser253
 
 import java.io.File
+import utilFunctions.{BiomeDBRelations, TransactionSupport}
+
 import scala.collection.JavaConverters._
 
 /**
   * Created by artem on 16.06.16.
   */
-class IntactUtil(dir: String) {
+class IntactUtil(dir: String, dataBaseFile: File) extends TransactionSupport {
   val fl = new File(dir)
   val reader = new PsimiXmlReader()
   val readResult = reader.read(fl).getEntries.asScala
+  val graphDataBaseConnection = new GraphDatabaseFactory().newEmbeddedDatabase(dataBaseFile)
+  var gbs = scala.collection.mutable.Set[String]()
+  var mapOfReactants = Map[Int, Long]()
 
   def getInteractors = {
-    readResult.map(_.getInteractors.asScala)
+    readResult.map(_.getInteractors.asScala).toList.flatten
   }
 
   def getInteractions = {
-    readResult.map(_.getInteractions.asScala)
+    readResult.map(_.getInteractions.asScala).toList.flatten
   }
 
   def getExperiments = {
-    readResult.map(_.getExperiments.asScala)
+    readResult.map(_.getExperiments.asScala).toList.flatten
   }
 
   def interactorInfo(interactor: Interactor) = {
     val id = interactor.getId
-    val name = interactor.getNames.getFullName
-    val geneName = interactor.getNames.getAliases
-    val seq = interactor.getSequence
-    val xrefs = interactor.getXref
-    val seqNode = new Sequence(seq)
+//    println(id)
+    val name = Option(interactor.getNames.getFullName)
+//    val geneName = interactor.getNames.getAliases
+    val seq = Option(interactor.getSequence)
+    val sequence = seq match {
+      case Some(s) => s
+      case None => println("No sequence: " + id + " " + name)
+    }
 
-    List(id, seqNode)
+    def makeMapOfXrefs(refs: List[DbReference], xrefMap: Map[String, String]): Map[String, String] = {
+      if (refs.nonEmpty) {
+        def makeXRef(ref: DbReference): Map[String, String] = ref.getId.contains(':') match {
+          case true => Map(ref.getDb -> ref.getId.split(':')(1))
+          case _ =>
+            gbs.add(ref.getDb)
+            Map(ref.getDb -> ref.getId)
+        }
+        makeMapOfXrefs(refs.tail, xrefMap ++ makeXRef(refs.head))
+      }
+      else xrefMap
+  }
+    val xrefs = interactor.getXref.getAllDbReferences.asScala.toList
+//    val x = xrefs.getAllDbReferences.asScala.foreach(makeMapOfRefs)
+    var mapOfXrefs = makeMapOfXrefs(xrefs, Map())
+
+    (mapOfXrefs, id, name, seq)
   }
 
   def interactionInfo(interaction: Interaction) = {
@@ -55,4 +81,66 @@ class IntactUtil(dir: String) {
     List(expId, participantDetectionMethod, interactionDetectionMethod, expNames)
   }
 
+  def findPolypetidesInteractors(interactors: List[Interactor]) = {
+    val parsedInteractors = interactors.map(interactorInfo)
+
+    def findPolyNodeByXRef(xrefMap: Map[String, String]) = {
+      def makeQuery(db: String, id: String) = {
+        val query = "MATCH (db:DB)<-[:LINK_TO]-" +
+          "(:XRef{id:'" + id + "'})<-[:EVIDENCE]-" +
+          "(p:Polypeptide) " +
+          "WHERE db.name=~'(?i).*" + db + ".*' " +
+          "RETURN ID(p)"
+//        println(query)
+        val queryResults = graphDataBaseConnection.execute(query).asScala.toList
+        queryResults
+      }
+      val foundNodes = xrefMap.map(dbAndId => makeQuery(dbAndId._1, dbAndId._2))
+      foundNodes
+    }
+    val findExistingNodes = parsedInteractors.map(l => findPolyNodeByXRef(l._1))
+    findExistingNodes.flatten.map(_.map(_.asScala))
+  }
+
+  def createInteractorNodes(interactors: List[Interactor]): Unit = transaction(graphDataBaseConnection){
+    val foundExistingPolyNodes = findPolypetidesInteractors(interactors)
+    val zipped = interactors.zip(foundExistingPolyNodes)
+    zipped.foreach(z => processOneInteractor(z._1, z._2))
+    def processOneInteractor(interactor: Interactor, queryResult: List[scala.collection.mutable.Map[String, AnyRef]]): Unit = {
+      val info = interactorInfo(interactor)
+      val reactantNode = graphDataBaseConnection.createNode(DynamicLabel.label("Reactant"))
+      reactantNode.setProperty("Intact id", info._2)
+      info._3 match {
+        case Some(name) => reactantNode.setProperty("name", name)
+        case _ =>
+      }
+      mapOfReactants += (info._2 -> reactantNode.getId)
+      if (queryResult.nonEmpty) {
+        val n = queryResult.head.get("ID(p)") match {
+          case Some(polyId) =>
+            val polyNode = graphDataBaseConnection.getNodeById(polyId.toString.toLong)
+            reactantNode.createRelationshipTo(polyNode, BiomeDBRelations.isA)
+          case _ =>
+        }
+      }
+      else {
+        info._4 match {
+          case Some(seq) => reactantNode.setProperty("seq", seq)
+          case _ =>
+        }
+      }
+    }
+  }
+
+  def createReactionsNodes(interactions: List[Interaction]) = transaction(graphDataBaseConnection) {
+
+  }
+
+  private def checkDbName(db: String) = {
+    val outputDbName = db.length match {
+      case i if i < 3 => db.toUpperCase
+      case _ => db.capitalize
+    }
+    outputDbName
+  }
 }
